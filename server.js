c4229abb5886dbb3763a5ffb87e9d3b6184debba
Forellko <dragonforce97q@gmail.com>
2021-02-04 18:32:40 +0300

    Access Refresh Tokens

---
+++ server.js
@@ -0,0 +1,50 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const jwt = require('jsonwebtoken')
+
+app.use(express.json())
+
+const posts = [
+  {
+    username: 'Kyle',
+    title: 'firstPost',
+  },
+  {
+    username: 'Jim',
+    title: 'firstPost',
+  },
+]
+
+app.get('/posts', authenticateToken, (req, res) => {
+  res.json(posts.filter((post) => post.username === req.user.name))
+})
+
+app.post('/login', (req, res) => {
+  // Authenticate User
+
+  const username = req.body.username
+  const user = { name: username }
+
+  // $ node
+  // require('crypto').randomBytes(64).toString('hex')
+  const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET)
+  res.json({ accessToken: accessToken })
+})
+
+function authenticateToken(req, res, next) {
+  const authHeader = req.headers['authorization']
+  const token = authHeader && authHeader.split(' ')[1]
+  if (token == null) return res.sendStatus(401)
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+    if (err) return res.sendStatus(403)
+    req.user = user
+    next()
+  })
+}
+
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
+  console.log(PORT)
+})
